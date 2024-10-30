e15b85e9fea86dbb715056c922906d02862e6547
Lovro <lovro.posaric@gmail.com>
2024-10-30 20:17:37 +0100

    Clean up the showcase code from MainActivity. All UI is going to be written from scratch, we don't need the "Greeting" composable from the project template.

--- app/src/main/java/hr/foi/air/popapp/MainActivity.kt
+++ app/src/main/java/hr/foi/air/popapp/MainActivity.kt
@@ -3,19 +3,9 @@ package hr.foi.air.popapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 import hr.foi.air.popapp.ui.theme.aseColor
 
@@ -26,34 +16,9 @@ class MainActivity : ComponentActivity() {
             POPAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = aseColor) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var counter by remember { mutableStateOf(0) }
-        Text(text = "You've pressed a button $counter times")
-        Button(onClick = { counter++ }) {
-            Text(text = "Press me")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    POPAppTheme {
-        Greeting("Android")
     }
 }
