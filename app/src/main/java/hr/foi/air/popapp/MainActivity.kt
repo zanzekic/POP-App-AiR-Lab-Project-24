3071f380b23b471abf133d452c5e8f10a5e8a8cd
Lovro <lovro.posaric@gmail.com>
2024-11-05 14:16:35 +0100

    Use NavHostController. This object enables the navigation through the application. Using the NavHost composable function, navigation is enabled. Currently, user is navigated to the "entry" route which is defined as EntryPage component. Therefore, this commit changes nothing in the display of the application, but sets terrain for enabling navigation.

--- app/src/main/java/hr/foi/air/popapp/MainActivity.kt
+++ app/src/main/java/hr/foi/air/popapp/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import hr.foi.air.popapp.navigation.components.EntryPage
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 
@@ -16,7 +19,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             POPAppTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    EntryPage()
+                    val navController = rememberNavController()
+
+                    NavHost(navController, startDestination = "entry") {
+                        composable("entry") {
+                            EntryPage()
+                        }
+                    }
                 }
             }
         }
