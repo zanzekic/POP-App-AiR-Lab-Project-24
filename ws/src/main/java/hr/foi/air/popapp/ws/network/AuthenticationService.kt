337449ee2f8b255d0d48936420ad7d7ffe5b32ce
Lovro <lovro.posaric@gmail.com>
2024-11-07 00:52:34 +0100

    Implement login functionality in 'ws' and 'login_username_password'.
    Now, module 'login_username_password' performs actual login via web API.
    It has a small unit test which makes sure login is achievable. The unit test uses one of the standard mock users that the backend depends upon as well (mock user "dhuff").
    Mock login logic is removed from 'app' and its LoginPage - to be reimplemented.

--- ws/src/main/java/hr/foi/air/popapp/ws/network/AuthenticationService.kt
+++ ws/src/main/java/hr/foi/air/popapp/ws/network/AuthenticationService.kt
@@ -2,6 +2,8 @@ package hr.foi.air.popapp.ws.network
 
 import hr.foi.air.popapp.core.network.models.ResponseBody
 import hr.foi.air.popapp.core.network.models.SuccessfulResponseBody
+import hr.foi.air.popapp.ws.models.LoggedInUserData
+import hr.foi.air.popapp.ws.models.LoginBody
 import hr.foi.air.popapp.ws.models.RegistrationBody
 import hr.foi.air.popapp.ws.models.RegistrationResponse
 import retrofit2.Call
@@ -11,4 +13,7 @@ import retrofit2.http.POST
 interface AuthenticationService {
     @POST("auth/register")
     fun registerUser(@Body registerBody: RegistrationBody): Call<SuccessfulResponseBody<RegistrationResponse>>
+
+    @POST("auth/login")
+    fun loginUser(@Body loginBody: LoginBody): Call<SuccessfulResponseBody<LoggedInUserData>>
 }
