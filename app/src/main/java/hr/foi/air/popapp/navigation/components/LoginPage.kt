ad8874519df40b9076ea989441cf0f738bfdce31
Lovro <lovro.posaric@gmail.com>
2024-11-07 00:52:39 +0100

    Connect new login with LoginPage.
    LoginPage now logs user in over an actual API call.
    What's missing is handling data belonging to logged user, which would include storing their authentication tokens safely and using them in future network requests.

--- app/src/main/java/hr/foi/air/popapp/navigation/components/LoginPage.kt
+++ app/src/main/java/hr/foi/air/popapp/navigation/components/LoginPage.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hr.foi.air.popapp.core.login.LoginHandler
 import hr.foi.air.popapp.core.login.LoginOutcomeListener
+import hr.foi.air.popapp.core.login.LoginUserData
 import hr.foi.air.popapp.login_username_password.UsernamePasswordLoginHandler
 import hr.foi.air.popapp.login_username_password.UsernamePasswordLoginToken
 import hr.foi.air.popapp.ui.components.PasswordTextField
@@ -26,6 +27,7 @@ fun LoginPage(
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    var awaitingResponse by remember { mutableStateOf(false) }
     var errorMessage by remember {
         mutableStateOf("")
     }
@@ -65,15 +67,20 @@ fun LoginPage(
 
         StyledButton(
             label = "Login",
+            enabled = !awaitingResponse,
             onClick = {
                 val usernamePasswordToken = UsernamePasswordLoginToken(username, password)
 
+                awaitingResponse = true
+
                 loginHandler.handleLogin(usernamePasswordToken, object : LoginOutcomeListener {
-                    override fun onSuccessfulLogin(username: String) {
+                    override fun onSuccessfulLogin(loginUserData: LoginUserData) {
+                        awaitingResponse = false
                         onSuccessfulLogin()
                     }
 
                     override fun onFailedLogin(reason: String) {
+                        awaitingResponse = false
                         errorMessage = reason
                     }
                 })
