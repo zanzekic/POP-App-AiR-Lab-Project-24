8d88f55f0d2d857ab895157566a1d65803a32e8e
Lovro <lovro.posaric@gmail.com>
2024-11-19 17:42:58 +0100

    Use a simple Singleton to store user info after login.
    Use a simple Singleton to store user info after login.  Now, Auth Singleton stores most important information about the user after login.

--- app/src/main/java/hr/foi/air/popapp/navigation/components/HomePage.kt
+++ app/src/main/java/hr/foi/air/popapp/navigation/components/HomePage.kt
@@ -1,7 +1,9 @@
 package hr.foi.air.popapp.navigation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,10 +11,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hr.foi.air.popapp.context.Auth
 import hr.foi.air.popapp.ui.components.MenuItem
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 import java.util.*
@@ -25,14 +30,24 @@ fun HomePage(onMenuOptionSelected: (optionName: String) -> Unit) {
         "Invoices" to Icons.AutoMirrored.Filled.List,
     )
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(20.dp)
-    ) {
-        items(menuItems) { (text, icon) ->
-            MenuItem(text, icon) { onMenuOptionSelected(text.lowercase(Locale.getDefault())) }
+    Column(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+        Text(
+            text = "Welcome, ${Auth.loggedInUserData?.firstName ?: "[Unknown name]"}",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.inversePrimary,
+            textAlign = TextAlign.Center
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(20.dp)
+        ) {
+            items(menuItems) { (text, icon) ->
+                MenuItem(text, icon) { onMenuOptionSelected(text.lowercase(Locale.getDefault())) }
+            }
         }
     }
 }
