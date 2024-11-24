foivz/POP-App-AiR-Lab-Project

Display stores on a map. If user is a buyer without a store selected, now they are shown all existing stores on a map. For each store fetched by the view-model, a marker state is added to a "storeMarkers" list. Such handling of these marker states enables the application to render Marker instances as children of the OpenStreetMap Composable function. For each stored marker state, a Marker composable function is invoked, resulting in markers being visible. What this solution is missing is connecting markers and their respective stores.

app/src/main/java/hr/foi/air/popapp/viewmodels/StoresViewModel.kt
@@ -1,0 +1,38 @@
+package hr.foi.air.popapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import hr.foi.air.popapp.context.Auth
+import hr.foi.air.popapp.core.login.network.ResponseListener
+import hr.foi.air.popapp.core.login.network.models.ErrorResponseBody
+import hr.foi.air.popapp.core.login.network.models.SuccessfulResponseBody
+import hr.foi.air.popapp.ws.models.responses.Store
+import hr.foi.air.popapp.ws.request_handlers.GetStoresRequestHandler
+
+class StoresViewModel : ViewModel() {
+    private val _stores: MutableLiveData<List<Store>> = MutableLiveData<List<Store>>()
+    val stores: LiveData<List<Store>> = _stores
+
+    private val _errorMessage: MutableLiveData<String> = MutableLiveData("")
+    val errorMessage: LiveData<String> = _errorMessage
+
+    fun fetchStores() {
+        val requestHandler = GetStoresRequestHandler(Auth.loggedInUserData!!.jwt)
+        requestHandler.sendRequest(
+            object : ResponseListener<Store> {
+                override fun onSuccessfulResponse(response: SuccessfulResponseBody<Store>) {
+                    _stores.value = response.data.toList()
+                }
+
+                override fun onErrorResponse(response: ErrorResponseBody) {
+                    _errorMessage.value = response.message
+                }
+
+                override fun onNetworkFailure(t: Throwable) {
+                    _errorMessage.value = "Network error"
+                }
+            }
+        )
+    }
+}

zanzekic/POP-App-AiR-Lab-Project-24

app/src/main/java/hr/foi/air/popapp/viewmodels/StoresViewModel.kt
@@ -1,0 +1,38 @@
+package hr.foi.air.popapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import hr.foi.air.popapp.context.Auth
+import hr.foi.air.popapp.core.network.ResponseListener
+import hr.foi.air.popapp.core.network.models.ErrorResponseBody
+import hr.foi.air.popapp.core.network.models.SuccessfulResponseBody
+import hr.foi.air.popapp.ws.models.responses.Store
+import hr.foi.air.popapp.ws.request_handlers.GetStoresRequestHandler
+
+class StoresViewModel : ViewModel() {
+    private val _stores: MutableLiveData<List<Store>> = MutableLiveData<List<Store>>()
+    val stores: LiveData<List<Store>> = _stores
+
+    private val _errorMessage: MutableLiveData<String> = MutableLiveData("")
+    val errorMessage: LiveData<String> = _errorMessage
+
+    fun fetchStores() {
+        val requestHandler = GetStoresRequestHandler(Auth.loggedInUserData!!.jwt)
+        requestHandler.sendRequest(
+            object : ResponseListener<Store> {
+                override fun onSuccessfulResponse(response: SuccessfulResponseBody<Store>) {
+                    _stores.value = response.data.toList()
+                }
+
+                override fun onErrorResponse(response: ErrorResponseBody) {
+                    _errorMessage.value = response.message
+                }
+
+                override fun onNetworkFailure(t: Throwable) {
+                    _errorMessage.value = "Network error"
+                }
+            }
+        )
+    }
+}